8ff794e34c817e9c1b1811b25fe2ec0fbce000c8
sannidhi shetty <sannidhishetty9@gmail.com>
2026-04-30 15:03:35 -0400

    First commit

---
+++ src/test/java/com/example/AI_summarizer/AiSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
